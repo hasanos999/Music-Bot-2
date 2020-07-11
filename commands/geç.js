c121ab285b4eb77b6c2f70eb149db3d74721d0fc
Glitch (oyuncraft-rythem) <none>
2020-07-11 20:01:33 +0000

    🎇💨 Checkpoint ./commands/geç.js:9146631/1585

--- commands/geç.js
+++ commands/geç.js
@@ -5,11 +5,11 @@ module.exports = {
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!message.member.voice.channel)
-      return message.channel.send("Öncelikle Bir Ses Kanalına Katılmanız Gerekiyor!").catch(console.error);
+      return message.channel.send({embed: {"description": `  **"Öncelikle Bir Ses Kanalına Girmeniz Gerekiyor!"** <:crossmark_274c:706879392181125150>"  [${message.author}]"`, "color": "#ff2050"}}).catch(console.error);
     if (!serverQueue)
-      return message.channel.send("Oynatılan Şarkıyı Bulamadım Lütfen Şarkı Açın Yoksa Geçemem.").catch(console.error);
+      return message.channel.send({embed: {"description": `  **Oynatılan Şarkıyı Bulamadım Lütfen Şarkı Açın Yoksa Geçemem.**  <:crossmark_274c:706879392181125150> [${message.author}]"`, "color": "#ff2050"}}).catch(console.error);
 
     serverQueue.connection.dispatcher.end();
-    serverQueue.textChannel.send(`${message.author} ⏭ Dinlediğiniz Şarkıyı Geçtim`).catch(console.error);
+    serverQueue.textChannel.send({embed: {"description": `  **${message.author} ⏭ Dinlediğiniz Şarkıyı Geçtim**  <:pngresmi:706880851022970930> [${message.author}]"`, "color": "#ff2050"}}).catch(console.error);
   }
 };
