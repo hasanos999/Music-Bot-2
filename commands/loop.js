fc406ad5bf76ca8f73576a57c6826c41a0a54783
Glitch (r3lease-muzik-55) <none>
2020-07-23 12:20:25 +0000

    👀🍳 Checkpoint ./util/EvobotUtil.js:9146631/78 ./commands/loop.js:9146631/5 ./events/ready.js:9146631/395 ./util/eventLoader.js:9146631/190 ./events/message.js:9146631/673 ./shard.js:9146631/229 ./package.json:9146631/40

--- commands/loop.js
+++ commands/loop.js
@@ -9,7 +9,7 @@ module.exports = {
     if (!queue) return message.channel.send({embed: {"description": `**${message.author} Oynatılan Bir Şarkı Bulamadım.**`, "color": "BLUE"}}); 
     if (!canModifyQueue(message.member)) return;
     queue.loop = !queue.loop;
-    return queue.textChannel({embed: {"description": `**Döngü Sistemi ${queue.loop ? "**Açık**" : "**Kapalı**"}**`, "color": "BLUE"}}); 
+    return queue.textChannel.send({embed: {"description": `**Döngü Sistemi ${queue.loop ? "**Açık**" : "**Kapalı**"}**`, "color": "BLUE"}}); 
   }
 };
 //Oyun Craft Abone Ol R3lease Kalp
---
+++ events/ready.js
@@ -0,0 +1,14 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  setInterval(function() {}, 8000);
+  client.user.setPresence({
+    game: {
+    },
+    status: "İDLE"
+  });
+};
